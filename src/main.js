8d025b79e84dc50ee1990b3cf4fb1b401f1a8545
Diogo Korok <diogo.korok@flimbo.io>
2018-12-03 14:28:03 -0200

    add Vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+
+import "./plugins/vuetify";
 import store from "./store";
+import router from "./router";
+
+import App from "./App.vue";
+
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
